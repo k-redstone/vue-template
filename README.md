51704191734208a27f09294e8bba4be191321b93
k-redstone <che0618@naver.com>
2024-05-11 18:03:30 +0900

    Feat: Setting vue template

---
+++ README.md
@@ -0,0 +1,9 @@
+# Vue-Template
+
+## Using
+
+- vue-router
+- pinia
+- tailwind
+- eslint
+- prettier
---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <nav class="font-bold text-3xl">hi</nav>
+</template>
+
+<style scoped></style>
